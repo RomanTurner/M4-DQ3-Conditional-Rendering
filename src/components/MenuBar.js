ff351dd154c8dfa8c1b827ed5f7d0056af7deb76
Roman Turner <rn.turner@live.com>
2021-03-19 09:50:32 -0700

    commit tree be863ad60637cc94f7d25cf5b69f64991841dddf

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const MenuBar = (props) => {
-
+console.log(this.props.menuChange);
   /*
 
   The 'a' tags below are the menu items. Think about the way a menu 
@@ -13,25 +13,38 @@ const MenuBar = (props) => {
 
   */
 
+  
   return (
-    <div className="ui four item menu">
-      <a className="item active" id="profile">
-        <i className="user large icon" id="profile"/>
+    <div className='ui four item menu'>
+      <a className='item active' id='profile'>
+        <i
+          onClick={() => this.props.menuChange()}
+          className='user large icon'
+          id='profile'
+        />
       </a>
 
-      <a className="item" id="photo">
-        <i className="photo large icon" id="photo"/>
+      <a onClick={(e) => this.props.menuChange(e)} className='item' id='photo'>
+        <i className='photo large icon' id='photo' />
       </a>
 
-      <a className="item" id="cocktail">
-        <i className="cocktail large icon" id="cocktail"/>
+      <a
+        onClick={(e) => this.props.menuChange(e)}
+        className='item'
+        id='cocktail'
+      >
+        <i className='cocktail large icon' id='cocktail' />
       </a>
 
-      <a className="item" id="pokemon"> 
-        <i className=" themeisle large icon" id="pokemon"/>
+      <a
+        onClick={(e) => this.props.menuChange(e)}
+        className='item'
+        id='pokemon'
+      >
+        <i className=' themeisle large icon' id='pokemon' />
       </a>
     </div>
-  )
+  );
 
 }
 
